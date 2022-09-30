f29e33a932e1e22936acd0524c66e176a9a795e7
yellowBunny <zigggin3@gmail.com>
2022-09-30 15:43:21 +0200

    rebuild scraper code

--- src/domain/preprocessing.py
+++ src/domain/preprocessing.py
@@ -1,35 +1,20 @@
 from bs4 import BeautifulSoup
-from typing import List
 import re
-import pdb
 
 everything = set()
 
-def extract_text_using_tag(file, tag:str="span"):
-    soup = BeautifulSoup(file, "html.parser")
-    finded_tags = soup.find_all(tag)
-    tags_withou_repetitions = finded_tags[1::2] # start from first index and grap every second
-    return tags_withou_repetitions
 
 def find_title(texts):
     global everything
     for text in texts:
         everything.add(extract_title_from_string(str(text)))
-        
 
 def extract_title_from_string(string:str):
     pattern = "<span>|</span>"
     text = re.split(pattern, string)[1]
-    # pdb.set_trace()
     if "<i>" in text:
         text = text.replace("<i>", "odcinek: ").strip('</i>')
     return text
-
-
-def printAllTitles(titles):
-    for title in sorted(titles):
-        print(title)
-    print(len(titles))
 
 def main():
     file = "audioteka_new.html"
@@ -37,6 +22,17 @@ def main():
         finded_tags = extract_text_using_tag(f)
         find_title(finded_tags)
     printAllTitles(everything)
+
+def extract_text_using_tag(file, tag:str="span"):
+    soup = BeautifulSoup(file, "html.parser")
+    finded_tags = soup.find_all(tag)
+    tags_withou_repetitions = finded_tags[1::2] # start from first index and grap every second
+    return tags_withou_repetitions
+
+def printAllTitles(titles):
+    for title in sorted(titles):
+        print(title)
+    print(len(titles))
 
 if __name__ == "__main__":
     main()
--- src/domain/scraper.py
+++ src/domain/scraper.py
@@ -2,35 +2,44 @@ from selenium import webdriver
 import chromedriver_binary
 import time
 
-# https://medium.com/analytics-vidhya/using-python-and-selenium-to-scrape-infinite-scroll-web-pages-825d12c24ec7
-driver = webdriver.Chrome()
+SCROLL_PAUSE = 5
 
-my_url = "https://web.audioteka.com/pl/do-uslyszenia-w-klubie.html"
-driver.get(my_url)
-time.sleep(2)
-scroll_pause_time = 5
-screen_height = driver.execute_script("return window.screen.height;")
-i = 0
-print(screen_height)
+def get_driver(url):
+    driver = webdriver.Chrome()
+    driver.get(url)
+    time.sleep(2)
+    return driver
+
+def scroll_down_page(driver, counter):
+    screen_height = get_screen_height(driver)
+    driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=counter))
+    time.sleep(SCROLL_PAUSE)
+    scroll_height = driver.execute_script("return document.body.scrollHeight;")
+    print(counter, f" Screan height: {screen_height * counter}, {scroll_height}")
+    if (screen_height) * counter > scroll_height + screen_height:
+        return True
+
+def get_screen_height(driver):
+    return driver.execute_script("return window.screen.height;")
+    
+    
+def save_data_to_file(driver):
+    with open("audioteka_new.html", "w") as f:
+        f.write(driver.page_source)
 
 
-while True:
-    # scroll one screen height each time
-    driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
-    i += 1
-    time.sleep(scroll_pause_time)
-    # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
-    scroll_height = driver.execute_script("return document.body.scrollHeight;")  
-    # Break the loop when the height we need to scroll to is larger than the total scroll height
-    print(i,f" Wielkość ekranu: {screen_height * i}, {scroll_height}")
-    if (screen_height) * i > scroll_height + screen_height:
-        break
-print(f"summary counter: {i}")
+def main():
+    my_url = "https://web.audioteka.com/pl/do-uslyszenia-w-klubie.html"
+    driver = get_driver(my_url)
+    counter = 0
+    print(f"Screan height: {get_screen_height(driver)}")
+    while True:
+        if scroll_down_page(driver, counter):
+            break
+        counter += 1
+    save_data_to_file(driver)
+    driver.close()
 
 
-
-with open("audioteka_new.html", "w") as f:
-    f.write(driver.page_source)
-    f.write(f"summary counter: {i}")
-
-driver.close()
+if __name__ == "__main__":
+    main()
