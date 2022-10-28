0f347fdf033625de324ddf9fc969ce1159cd00ab
yellowBunny <zigggin3@gmail.com>
2022-10-28 10:15:05 +0200

    add only events for Title entity

--- src/domain/model.py
+++ src/domain/model.py
@@ -1,8 +1,10 @@
 class Title:
     def __init__(self, title):
         self.title = title
+        self.events = []
 
     def __eq__(self, other):
         if not isinstance(other, Title):
             return False
         return self.title == other.title
+
