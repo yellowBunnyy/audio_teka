a7d043125f645dfdc56a7b83a83dcb168ba7d408
yellowBunnyy <zigggin3@gmail.com>
2023-02-06 22:35:57 +0100

    host can talk with docker api

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import os
 def get_postgres_uri():
     return os.environ.get("DB_URI")
 
-def get_api_url():
-    host = "localhost"
-    port = 5000
-    return f"http://{host}:{port}"
+# def get_api_url():
+#     host = "0.0.0.0"
+#     port = 5000
+#     return f"http://{host}:{port}"
